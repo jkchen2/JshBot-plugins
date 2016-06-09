dd93fb36a994c2912ce62fd25ba28d6c4d4def7e
jkchen2 <jkchen2@illinois.edu>
2016-06-09 15:15:48 -0500

    Dummy update
    This update adds a few more examples and explains a few more things. It's also now written in the new command syntax.

--- dummy/dummy.py
+++ dummy/dummy.py
@@ -1,4 +1,4 @@
-################################################################################
+###############################################################################
 #
 # NOTE: If you don't need this file, feel free to delete it! It only serves to
 #   be a demo of how plugins should be made.
@@ -10,7 +10,7 @@
 #   how it can be used.
 #
 # The file demonsrates how to create new commands with standard syntax,
-#   as well as commands and help (manual entry).
+#   as well as shortcuts and help (manual entry).
 #
 # There is also a demonstration on how get_response() should be used, and how
 #   to read a parsed result.
@@ -19,80 +19,109 @@
 #   define standard events from discord.Client, and they will be called
 #   appropriately.
 #
-################################################################################
+###############################################################################
 
-import discord
 import asyncio
 
+from jshbot.commands import Command, SubCommands, Shortcuts
 from jshbot.exceptions import BotException
 
 __version__ = '0.1.0'
 EXCEPTION = 'Dummy'
 uses_configuration = False
 
+
 def get_commands():
+    """Returns a list of commands associated with this plugin."""
+    commands = []
 
-    commands = {}
-    shortcuts = {}
-    manual = {}
+    commands.append(Command(
+        'mycommand', SubCommands(
+            ('myoption', 'myoption', 'This is a simple command with a single '
+             'required option.'),
+            ('?custom ?attached:', '(custom) (attached <"attached argument">)',
+             'This has two different optional options, one without an '
+             'attached argument, and the other requiring an attached argument '
+             'if the option \'attached\' is specified.'),
+            ('trailing ::+', 'trailing <"arg 1"> <"arg 2"> <"arg 3"> '
+             '("arg 4") ("arg 5") (...)', 'This command requires lot of '
+             'trailing arguments.'),
+            ('grouped ^', 'grouped <grouped arguments>', 'This will group all '
+             'given arguments as a single string.'),
+            ('complex: ?other: :#', 'complex <"attached"> (other <"also '
+             'required">) <"arg 1"> ("arg 2") ("arg 3") (...)', 'The complex '
+             'option has a required positional argument, and the \'other\' '
+             'option has one too if it is specified. Additionally, there will '
+             'be a requirement of at least 1 trailing arguent.'),
+            ('marquee ^', 'marquee <text>', 'Create a marquee that loops '
+             'three times.')),
+        shortcuts=Shortcuts(
+            ('complex', 'complex {} other {} {} {}', ':::#',
+             '<"attached"> other <"other"> <"arg 1"> ("arg 2") (...)',
+             '<"attached"> <"other"> <"arg 1"> ("arg 2") (...)'),
+            ('marquee', 'marquee {}', '^',
+             'marquee <text>', '<text>')),
+        description='Your command description here.',
+        other='This text is optional - it just shows up after everything '
+              'else. Quick note, all of the commands here can only be used by '
+              'bot moderators or above, as indicated by elevated_level. A '
+              'level of 2 would mean only server owners or above can use the '
+              'command, and a level of 3 would restrict the command to only '
+              'the bot owners.',
+        elevated_level=1))
 
-    # Commands
-    # (For more help on these symbols, refer to command_reference.txt)
-    commands['mycommand'] = ([
-        'myoption',
-        '?custom ?attached:',
-        'trailing ::+',
-        'grouped ^',
-        'complex: ?other: :#',
-        'marquee ^'],[
-        ('myoption', 'option', 'o'),
-        ('trailing', 'trail'),
-        ('grouped', 'blocked')])
-    commands['myothercommand'] = (['&'], [])
+    commands.append(Command(
+        'myothercommand', SubCommands(
+            ('&', '<text>', ''),
+            ('order matters', 'order matters', 'It is impossible to access '
+             'this command because the first subcommand will always be '
+             'satisfied first. Order of subcommands matters!'),
+            ('sample foo bar', 'sample foo bar', 'Also impossible to access. '
+             'This subcommand just adds some keywords to the command.')),
+        description='Only bot owners can see this text!',
+        other='Note that no shortcuts were defined. They, too, are optional. '
+              'Also, this command is hidden, which means that only the bot '
+              'owners can see this command listed from the help command. '
+              'However, unless the command is configured with an elevated '
+              'permissions level, any user can still execute the command. '
+              'Users still will not be able to see the specific help for this '
+              'command, though. Lastly, this command is disabled in DMs.',
+        hidden=True, allow_direct=False))
 
-    # Shortcuts
-    shortcuts['complex'] = ('mycommand -complex {} -other {} {} {}', ':::#')
-    shortcuts['group'] = ('mycommand -grouped {}', '^')
-    shortcuts['myshortcut'] = ('myothercommand', '')
+    commands.append(Command(
+        'notify', SubCommands(('^', '<text>', 'Notify the owners!')),
+        other='This command uses a custom function. It is called with the '
+              'same arguments as get_response. The command will show up to '
+              'all users in the help command, but can only be used by server '
+              'owners, as it is disallowed in direct messages.',
+        elevated_level=2, allow_direct=False, function=custom_notify))
 
-    # Manual
-    manual['mycommand'] = {
-        'description': 'Your command description here.',
-        'usage': [
-            ('-myoption', 'This is a simple command with a single required '
-                'option.'),
-            ('(-custom) (-attached <attached argument>)', 'This has two '
-                'different optional options, one without a flag, and the other '
-                'requring an attached argument if it the flag is specified.'),
-            ('-trailing <arg 1> <arg 2> <arg 3> (arg 4) (arg 5) (...)', 'This '
-                'command has a lot of trailing arguments as a requirement.'),
-            ('-grouped <grouped arguments>', 'This will group all given '
-                'arguments as a single string.'),
-            ('-complex <attached> (-other <also required>) <arg 1> (arg 2) '
-                '(arg 3) (...)', 'The complex flag has a required associated '
-                'positional argument, and the other flag has one too if it is '
-                'specified. Additionally, there will be a requirement of at '
-                'least 1 trailing argument.'),
-            ('-marquee <text>', 'Creates a marquee that loops three times.')],
-        'shortcuts': [
-            ('complex <attached> <other> <arg1> (arg2) (...)',
-            '-complex <attached> -other <other> <arg1> (arg2) (...)'),
-            ('group <argument>', '-group <argument>')],
-        'other': 'All of this text is actually not required! You can choose '
-            'to not include any of these sections, and they will simply not '
-            'show up. Obviously, it\'s best if at least the description and '
-            'usage portions were provided.'}
-    manual['myothercommand'] = {
-        'description': 'Just another basic command.'}
+    return commands
 
-    # Returned as a touple - the core will handle the rest
-    return (commands, shortcuts, manual)
 
-async def get_response(bot, message, parsed_command, direct):
+async def get_response(
+        bot, message, base, blueprint_index, options, arguments,
+        keywords, cleaned_content):
+    """Gets a response given the parsed input.
 
+    Arguments:
+    bot -- A reference to the bot itself.
+    message -- The discord.message object obtained from on_message.
+    base -- The base command name that immediately follows the invoker.
+    blueprint_index -- The index of the subcommand of the given base command.
+    options -- A dictionary representing the options and potential positional
+        arguments that are attached to them.
+    arguments -- A list of strings that follow the syntax of the blueprint
+        index for arguments following the options.
+    keywords -- Another list of strings that holds all option keywords. These
+        can be used to prevent database conflicts with user commands.
+    cleaned_content -- Simply the message content without the invoker.
+    """
+
+    # This is what the bot will say when it returns from this function.
     response = ''
 
-    # Set to True if you want your message read with /tts (not recommended)
+    # Set to True if you want your message read with /tts (not recommended).
     tts = False
 
     # The message type dictates how the bot handles your returned message.
@@ -112,103 +141,120 @@ async def get_response(bot, message, parsed_command, direct):
     #
     #   3 - Active message. This message will be passed back to the plugin for
     #       extra processing and editing. The function it will call is
-    #       handle_active_message(message_reference, extra). See the comments
-    #       for handle_active_message for more information.
+    #       handle_active_message(). See the comments for
+    #       handle_active_message() for more information.
     #
     message_type = 0
 
     # The extra variable is used for the second and third message types.
     extra = None
 
-    # This is what the parser returns that you can use.
-    #   base - The base command that immediately follows the command invoker.
-    #   plan_index - The index of the plan as specified by the commands
-    #       dictionary in get_commands.
-    #   options - The dictionary of the given options, and their arguments if
-    #       they have any.
-    #   arguments - The list or string of arguments found after the command.
-    base, plan_index, options, arguments = parsed_command
-
     # Initially, check to make sure that you've matched the proper command.
     # If there is only one command specified, this may not be necessary.
     if base == 'mycommand':
 
-        # Then, the plan_index will tell you which command syntax was
+        # Then, the blueprint_index will tell you which command syntax was
         #   satisfied. The order is the same as was specified initially.
-        if plan_index == 0: # myoption
-            response = "You called the first command!"
-            if direct:
-                response += "This is called in a direct message!"
+        if blueprint_index == 0:  # myoption
+            response = "You called the first subcommand!"
             # Do other stuff...
 
-        elif plan_index == 1: # custom/attached
-
+        elif blueprint_index == 1:  # custom/attached
             # To see if an optional option was included in the command, use:
             if 'custom' in options:
                 response += "You included the \"custom\" flag!\n"
-                # Do stuff relevant to this flag...
+                # Do stuff relevant to this flag here...
 
             # To get the argument attached to an option, simply access it from
             #   the options dictionary.
             if 'attached' in options:
-                response += ("The attached argument: " + options['attached'] +
-                    '\n') # Can't believe this is on a line by itself.
+                response += "The attached argument: {}\n".format(
+                    options['attached'])
 
             # In case somebody was looking for the help...
             if len(options) == 0:
+                invoker = bot.get_invoker(server=message.server)
                 response += "You didn't use either flag...\n"
-                response += "For help, try `{}help mycommand`".format(
-                        bot.configurations['core']['command_invokers'][0])
+                response += "For help, try `{}help mycommand`".format(invoker)
 
-        elif plan_index == 2: # trailing arguments
-
+        elif blueprint_index == 2:  # trailing arguments
             # If arguments are specified as trailing, they will be in a list.
-            response += "The list of trailing arguments: " + str(arguments)
+            response += "The list of trailing arguments: {}".format(arguments)
 
-        elif plan_index == 3: # grouped arguments
+        elif blueprint_index == 3:  # grouped arguments
+            # All arguments are grouped together as the first element
+            message_type = 1
+            response = ("You can't edit your command here.\n"
+                        "Single grouped argument: {}").format(arguments[0])
 
-            # If arguments are specified as grouped, it will be a single string.
-            response += "Here is the grouped argument: " + arguments
-
-        elif plan_index == 4: # complex
-
+        elif blueprint_index == 4:  # complex
             # This mixes elements of both examples seen above.
-            response = ("The argument attached to the complex option: " +
-                    options['complex'] + '\n')
+            response = ("The argument attached to the complex "
+                        "option: {}\n").format(options['complex'])
             if 'other' in options:
-                response += "The other option has: " + options['other'] + '\n'
-            response += "Lastly, the trailing arguments: " + str(arguments)
+                response += "The other option has attached: {}\n".format(
+                    options['other'])
+            response += "Lastly, the trailing arguments: {}".format(arguments)
 
-        elif plan_index == 5: # (Very slow) marquee
-
+        elif blueprint_index == 5:  # (Very slow) marquee
             # This demonstrates the active message type. Check
             #   handle_active_message to see how it works.
-            if not arguments or len(arguments) > 100 or '\n' in arguments:
+            text = arguments[0]
+            if not text or len(text) > 100 or '\n' in text:
                 response = ("Must have text 1-100 characters long, and must "
-                        "not have any new lines.")
+                            "not have any new lines.")
             else:
-                message_type = 3 # active
-                extra = ('marquee', arguments)
-                response = "Setting up marquee..."
+                message_type = 3  # active
+                extra = ('marquee', text)
+                response = "Setting up marquee..."  # This will be shown first
 
     # Here's another command base.
     elif base == 'myothercommand':
 
-        # We only have one command, checking for plan_index isn't necessary.
-        if arguments:
-            response = ("You called the other command and gave this "
-                    "argument: " + arguments)
-        else:
-            response = "You called the other command with no arguments."
+        if blueprint_index == 0:  # keyword checker
+            text = arguments[0]
+            if not text:
+                response = "You didn't say anything...\n"
+            else:
+                response = "This is your input: {}\n".format(text)
+                if text in keywords:
+                    response += "Your input was in the list of keywords!\n"
+                else:
+                    response += ("Your input was not in the list of keywords. "
+                                 "They are: {}\n").format(keywords)
+
+            message_type == 2  # Self-destruct
+            extra = 15  # 15 seconds
+            response += "This message will self destruct in 15 seconds."
+
+        else:  # impossible command???
+            raise BotException(
+                EXCEPTION, "This is a bug! You should never see this message.")
 
     return (response, tts, message_type, extra)
 
+
+async def custom_notify(bot, message, *args):
+    """This is only called with the notify command.
+
+    This function is called with the same arguments as get_response.
+    """
+    response, tts, message_type, extra = ('', False, 0, None)
+
+    notify_message = '{0} from server {1} is sending you: {2}'.format(
+        message.author.display_name, message.server.name, args[3][0])
+    await bot.notify_owners(notify_message)
+    response = "Notified the owners with your message!"
+
+    return (response, tts, message_type, extra)
+
+
 async def handle_active_message(bot, message_reference, extra):
-    '''
-    This function is called if the given message was marked as active
+    """This is called if the given message was marked as active.
+
     (message_type of 3).
-    '''
-    if extra[0] == 'marquee': # Handle the marquee active message
+    """
+    if extra[0] == 'marquee':  # Handle the marquee active message
 
         # Set text expanded with whitespace
         total_length = 40 + len(extra[1])
@@ -218,12 +264,13 @@ async def handle_active_message(bot, message_reference, extra):
         for it in range(3):
             for move in range(total_length - 20):
                 moving_text = '`|{:.20}|`'.format(text[move:])
-                await asyncio.sleep(1) # Don't get rate limited!
+                await asyncio.sleep(1)  # Don't get rate limited!
                 await bot.edit_message(message_reference, moving_text)
 
         # When the marquee is done, just display the text
         await asyncio.sleep(1)
         await bot.edit_message(message_reference, extra[1])
+
 
 # If necessary, discord.Client events can be defined here, and they will be
 #   called appropriately. Be sure to include the bot argument first!
@@ -232,6 +279,6 @@ async def on_ready(bot):
     print("on_ready was called from dummy.py!")
 
 async def on_message_edit(bot, before, after):
-    print("Somebody edited their message from '{}' to '{}'.".format(
-        before.content, after.content))
-
+    if before.author != bot.user:
+        print("Somebody edited their message from '{0}' to '{1}'.".format(
+            before.content, after.content))
--- experiments/experiments.py
+++ experiments/experiments.py
@@ -10,8 +10,8 @@ import youtube_dl
 
 import datetime
 
-from tinytag import TinyTag
 from jshbot import data, utilities
+from jshbot.commands import Command, SubCommands, Shortcuts
 from jshbot.exceptions import BotException
 
 __version__ = '¯\_(ツ)_/¯'
@@ -20,55 +20,118 @@ uses_configuration = False
 
 
 def get_commands():
+    commands = []
+    commands.append(Command(
+        'test', SubCommands(
+            ('order', 'order', 'This should have higher priority.'),
+            ('shortcutone', 'shortcutone', ''),
+            ('shortcuttwo', 'shortcuttwo', ''),
+            ('^', '(aesthetic)', 'Make text aesthetic. Or do the test '
+             'function. Up to you, really.')),
+        shortcuts=Shortcuts(
+            ('short1', 'shortcutone', '', 'shortcutone', '')),
+        description='Testing commands.', elevated_level=3, hidden=True))
 
-    commands = {}
-    shortcuts = {}
-    manual = {}
+    commands.append(Command(
+        'test2', SubCommands(
+            ('?args: :::::^', '(args <attached>) <arg1> <arg2> <the rest>',
+             'Argument testing.'),
+            ('nope: :#', 'nope <nope attached> <arg1> (arg2) (arg3) ...',
+             'More argument testing'),
+            ('junk stuff', 'junk stuff', 'Even more...'),
+            ('final test please', 'final test please', 'blargh')),
+        description='Testing commands 2: Electric Boogaloo.',
+        elevated_level=3))
 
-    commands['test'] = (['&'], [()])
-    commands['timemasheen'] = (['^'], [()])
-    commands['play'] = (['?file ^'], [()])
-    commands['volume'] = (['&'], [()])
-    commands['rip'] = (['^'], [()])
-    commands['nuke'] = (['^'], [()])
+    commands.append(Command(
+        'timemasheen', SubCommands(
+            ('^', '<dd/mm/yy>', 'Retrieves chat logs on the given day.')),
+        description='Carter\'s time machine.', other='Nice meme!'))
 
-    return (commands, shortcuts, manual)
+    commands.append(Command(
+        'play', SubCommands(
+            ('?file ^', '(file) <url or file>',
+             'Plays the given URL or file.')),
+        description='Plays stuff using youtube-dl. Neat.', allow_direct=False))
 
-async def get_response(bot, message, parsed_command, direct):
-    response = ''
-    tts = False
-    message_type = 0
-    extra = None
-    base, plan_index, options, arguments = parsed_command
+    commands.append(Command(
+        'volume', SubCommands(
+            ('&', '<volume>', 'Sets the volume of the experiments player.')),
+        shortcuts=Shortcuts(
+            ('volumeshortcut', '{}', '^', '<volume>', '<volume>')),
+        other='Volume must be between 0.1 and 2.0', allow_direct=False))
+
+    commands.append(Command(
+        'rip', SubCommands(('^', '<thing>', 'Rips a thing.'))))
+
+    commands.append(Command(
+        'nuke', SubCommands(
+            ('^', '<number of messages>', 'Deletes the specified number of '
+             'messages, including the authoring message.')),
+        description='Deletes messages.', other='Be careful with this!',
+        elevated_level=1, allow_direct=False))
+
+    return commands
+
+async def get_response(
+        bot, message, base, blueprint_index, options, arguments,
+        keywords, cleaned_content):
+    response, tts, message_type, extra = ('', False, 0, None)
 
     if base == 'test':
-        if arguments:  # that's pretty aesthetic mate
-            text = arguments.replace(' ', '').lower()
+
+        if blueprint_index == 0:  # order
+            response = "Ordered!"
+
+        elif blueprint_index == 1:  # shortcut 1
+            response = "You reached shortcut 1!"
+
+        elif blueprint_index == 2:  # shortcut 2
+            response = "You reached shortcut 2!"
+
+        elif blueprint_index == 3:  # that's pretty aesthetic mate
+            text = arguments[0].replace(' ', '').lower()
             response = ' '.join([char for char in text])
             await bot.notify_owners(
-                "Somebody made {} aesthetic.".format(arguments))
+                "Somebody made {} aesthetic.".format(arguments[0]))
 
         else:  # asyncio testing
             long_future = bot.loop.run_in_executor(None, long_function)
             await long_future
             response = "Finished sleeping"
 
+    elif base == 'test2':
+        if blueprint_index == 0:
+            if 'args' in options:
+                response = "Args was included: {}\n".format(options['args'])
+            response += str(arguments)
+        elif blueprint_index == 1:
+            response = "Nope\n"
+            response += str(options) + '\n'
+            response += str(arguments)
+        elif blueprint_index == 2:
+            assert False
+        else:
+            response = str(options) + '\n'
+            response += str(arguments) + '\n'
+            response = "Blah blah, empty response."
+
     elif base == 'rip':
-        response = get_rip(arguments)
+        response = get_rip(arguments[0])
 
     elif base == 'nuke':
         if not data.is_owner(bot, message.author.id):
             raise BotException(
                 EXCEPTION, "Can't nuke unless you're the owner.")
-        limit = int(arguments) + 1
+        limit = int(arguments[0]) + 1
         await bot.purge_from(message.channel, limit=limit)
 
     elif base == 'timemasheen':  # carter's time masheen
         for delimiter in ('/', '.', '-'):
-            if delimiter in arguments:
+            if delimiter in arguments[0]:
                 break
         start_date = datetime.datetime.strptime(
-            arguments, '%d{0}%m{0}%y'.format(delimiter))
+            arguments[0], '%d{0}%m{0}%y'.format(delimiter))
         end_date = start_date + datetime.timedelta(days=1)
         await send_logs_as_file(bot, message.channel, start_date, end_date)
         message_type = 1
@@ -78,17 +141,15 @@ async def get_response(bot, message, parsed_command, direct):
         if voice_channel:
             use_file = 'file' in options
             await play_this(
-                bot, message.server, voice_channel, arguments, use_file)
+                bot, message.server, voice_channel, arguments[0], use_file)
             response = "Playing your stuff."
         else:
             raise BotException(EXCEPTION, "You're not in a voice channel.")
 
     elif base == 'volume':  # change volume
-        player = data.get(
-            bot, __name__, 'voice_client',
-            server_id=message.server.id, volatile=True)
+        player = utilities.get_player(bot, message.server.id)
         if arguments:
-            volume = float(arguments)
+            volume = float(arguments[0])
             if volume < 0 or volume > 2:
                 raise BotException(EXCEPTION, "Valid range is [0.0-2.0].")
             else:
@@ -100,6 +161,9 @@ async def get_response(bot, message, parsed_command, direct):
         if player:
             player.volume = volume
 
+    else:
+        response = "You forgot to set up the test command, you dummy!"
+
     return (response, tts, message_type, extra)
 
 
@@ -110,7 +174,14 @@ def get_rip(name):
         'rip in pizza, {}',
         'press f to pay respects to {}',
         '{} will be in our hearts',
-        '{} didn\'t stand a chance'
+        '{} didn\'t stand a chance',
+        '{} is kill',
+        '{} got destroyed',
+        '{} got rekt',
+        '{} got noscoped',
+        'it is sad day, as {} has been ripped',
+        '{} got tactical nuked',
+        '{} couldn\'t handle the mlg'
     ]
     return random.choice(rip_messages).format(name)
 
