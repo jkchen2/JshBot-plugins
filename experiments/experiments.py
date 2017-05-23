ac723a66c1f09830edb0255ebb9b8e1ca1078c87
jkchen2 <jkchen2@illinois.edu>
2017-05-23 18:51:12 -0500

    Test gif creation
    Test the creation of shaky gifs. Includes support for tinting, adding text, and a custom intensity level. This will likely eventually be put into its own meme plugin.

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -8,8 +8,10 @@ import time
 import logging
 import youtube_dl
 import subprocess
-
+import requests
 import datetime
+import PIL
+import io
 
 from jshbot import data, utilities, configurations
 from jshbot.commands import Command, SubCommands, Shortcuts
@@ -24,6 +26,13 @@ print("Loaded experiments")
 
 def get_commands():
     commands = []
+
+    commands.append(Command(
+        'zoop', SubCommands(
+            ('^', '<stuff>', 'Help string here.')),
+        shortcuts=Shortcuts(
+            ('zap', '{}', '^', '<stuff>', '<stuff>'))))
+
     commands.append(Command(
         'test', SubCommands(
             ('order', 'order', 'This should have higher priority.'),
@@ -79,6 +88,20 @@ def get_commands():
              'messages, including the authoring message.')),
         description='Deletes messages.', other='Be careful with this!',
         elevated_level=1, allow_direct=False, group='testing'))
+
+    commands.append(Command(
+        'wh', SubCommands((':&', '<"text"> (color)', 'Stuff.')),
+        elevated_level=3, group='testing'))
+
+    commands.append(Command(
+        'embed', SubCommands(('&', '(embed dictionary)',
+        'Tests an embed object.')), group='testing'))
+
+    commands.append(Command(
+        'animate', SubCommands(
+            ('?level: ?gradual: ?tint: ?text: ^', '(level <intensity>) '
+             '(gradual <duration>) (tint <color>) (text <text>) <url>', '')),
+            group='testing'))
 
     return commands
 
@@ -193,10 +216,208 @@ async def get_response(
         if player:
             player.volume = volume
 
+    elif base == 'wh':  # Webhooks
+        import json
+        webhook_url = "https://canary.discordapp.com/api/webhooks/249456349728079872/1wKqRf6fF5h8MkubiWrSZGjc6hi8r_DFzggSLETejUawMoWxbQCtTyMwTKoTndNuJtgW"
+        '''
+        sent_data = {
+            "embeds": [
+                {
+                    "color": (int(arguments[1]) if arguments[1] else 16777215),
+                    "description": arguments[0],
+                    "image": {
+                        "url": (
+                            "https://cdn.discordapp.com/attachments/"
+                            "220327974434504705/234558750860509186/result.png")
+                    }
+                }
+            ]
+        }
+        '''
+        sent_data = {'content': arguments[0]}
+        headers = {'content-type': 'application/json'}
+        r = requests.post(
+            webhook_url, data=json.dumps(sent_data), headers=headers)
+        bot.extra = r
+        response = "POST sent"
+
+    elif base == 'embed':  # Embedded message type
+        if arguments[0]:  # Test embed object
+            import ast
+            try:
+                test_arguments = ast.literal_eval(arguments[0])
+            except Exception as e:
+                raise BotException(EXCEPTION, "Not a valid dictionary.", e=e)
+
+        else:  # Default example in discordpy documentation
+            test_arguments = {
+                'title': None,
+                #'title': 'My Embed Title',
+                'description': 'My Embed Content.',
+                'colour': 0xFFFFFF,  # Originally 0xDEADBF
+                'timestamp': message.timestamp
+            }
+        response = discord.Embed(**test_arguments)
+        response.set_author(
+            name='Someone', icon_url=bot.user.default_avatar_url)
+        response.add_field(name='name1', value='value1')
+        response.add_field(name='_', value='_', inline=False)
+        response.add_field(name='name3', value='value3')
+        response.add_field(name='name4', value='value4')
+        response.add_field(name='name5', value='value5')
+        response.add_field(name='name6', value='value6')
+        response.add_field(name='name7', value='value7')
+        response.set_image(url='https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png')
+        response.set_thumbnail(url='https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png')
+        response.set_footer(text='This is a test footer')
+
+    elif base == 'animate':  # Animation test
+        # response = await utilities.future(_animation_test, bot)
+        url = arguments[0]
+        kwargs = {}
+        if 'level' in options:
+            try:
+                level = float(options['level'])
+                if not 1 <= level <= 10:
+                    raise ValueError
+                kwargs['level'] = level
+            except ValueError:
+                raise BotException(
+                    EXCEPTION, "Level must be between 1 and 10 inclusive.")
+        if 'gradual' in options:
+            try:
+                duration = int(options['gradual'])
+                if not 1 <= duration <= 3:
+                    raise ValueError
+                kwargs['duration'] = duration
+                kwargs['gradual'] = True
+            except ValueError:
+                raise BotException(
+                    EXCEPTION, "Gradual must be between 1 and 3 inclusive.")
+        if 'tint' in options:
+            try:
+                clean_tint = options['tint'].replace('0x', '').replace('#', '')
+                if len(clean_tint) == 6:
+                    clean_tint += '80'
+                tint_test = int(clean_tint, 16)
+                if tint_test > 0xffffffff or len(clean_tint) != 8:
+                    raise ValueError
+                kwargs['tint'] = clean_tint
+            except ValueError:
+                raise BotException(
+                    EXCEPTION, "Tint must be a (full-length RGBA) hex color.")
+        if 'text' in options:
+            if len(options['text']) > 30:
+                raise BotException(
+                    EXCEPTION, "Text must be less than 30 characters long.")
+            kwargs['text'] = options['text']
+
+        response = await _animation_test(bot, url, **kwargs)
+        message_type = 5
+        response.seek(0)
+        extra = 'why.gif'
+
     else:
         response = "You forgot to set up the test command, you dummy!"
 
     return (response, tts, message_type, extra)
+
+
+async def _animation_test(
+        bot, url, level=3, duration=1, tint=None, text='', gradual=False):
+    file_location, cleaned_name = await utilities.download_url(
+        bot, url, include_name=True)
+    try:
+        base_image = PIL.Image.open(file_location).convert("RGBA")
+    except Exception as e:
+        raise BotException(EXCEPTION, "Failed to open the image.", e=e)
+    base_size = base_image.size
+    if max(base_size) > 200:  # Resize so largest dimension is 200px
+        ratio = 200/max(base_size)
+        base_image = base_image.resize(
+            (int(ratio * base_size[0]), int(ratio * base_size[1])))
+        base_size = base_image.size
+    border_ratio = 1 - (0.5 * level / 10)
+    border_size = (
+        int(border_ratio * base_size[0]), int(border_ratio * base_size[1]))
+    max_offset = (
+        int((1 - border_ratio) * base_size[0]),
+        int((1 - border_ratio) * base_size[1]))
+
+    if tint is not None:
+        tint = tuple(int(it0, 16) for it0 in [
+            tint[it1:it1 + 2] for it1 in range(0, 8, 2)])
+
+    def _composite_tint(original_tint, given_tint):
+        ratio = given_tint[3]
+        return tuple(
+            max((int(a + b * ratio), 255)) for a,b in zip(
+                original_tint, given_tint))
+
+    def _process_images():
+        frames = []
+        ref_image = base_image
+        offsets = max_offset
+        preoffsets = (0, 0)
+        dark_tint = (54, 57, 62, 255)
+        tint_step = tint
+        if text:
+            font_directory = (
+                bot.path + '/plugins/plugin_data/OpenSans-CondBold.ttf')
+            font = PIL.ImageFont.truetype(font_directory, 20)
+            text_dummy = PIL.ImageDraw.Draw(base_image)
+            text_size = text_dummy.textsize(text, font=font)
+            text_image = PIL.Image.new('RGBA', (text_size), (0,)*4)
+            text_draw = PIL.ImageDraw.Draw(text_image)
+            text_draw.text((0, 0), text, font=font, fill=(255,)*4)
+            if (max(text_size) + 10) > max(border_size):
+                ratio = max(border_size)/max(text_size)
+                text_image = text_image.resize(
+                    (int(ratio * text_size[0]), int(ratio * text_size[1])))
+            text_size = text_image.size
+            
+
+        for it in range(duration * 50):
+            if gradual:
+                progress = it / (duration * 50)
+                ratio = 0.5 + 0.5 * progress
+                offsets = (
+                    int(progress * max_offset[0]),
+                    int(progress * max_offset[1]))
+                ref_image = base_image.resize(
+                    (int(ratio * base_size[0]), int(ratio * base_size[1])))
+                preoffsets = (
+                    int(border_size[0]/2 - (ref_image.size[0] - offsets[0])/2),
+                    int(border_size[1]/2 - (ref_image.size[1] - offsets[1])/2))
+                if tint is not None:
+                    tint_step = tuple(int(it * progress) for it in tint)
+            new_image = PIL.Image.new('RGBA', border_size, dark_tint)
+            new_image.paste(ref_image,
+                (
+                    preoffsets[0] - random.randint(0, offsets[0]),
+                    preoffsets[1] - random.randint(0, offsets[1])
+                ), ref_image
+            )
+            if tint:
+                tint_image = PIL.Image.new('RGBA', border_size, tint_step)
+                new_image.paste(tint_image, (0, 0), tint_image)
+            if text:
+                text_alignment = (
+                    int(border_size[0]/2 - (text_size[0])/2),
+                    int(border_size[1] - text_size[1]))
+                new_image.paste(text_image, text_alignment, text_image)
+
+            frames.append(new_image)
+        return frames
+    
+    frames = await utilities.future(_process_images)
+    image_bytes = io.BytesIO()
+    frames[0].save(
+        image_bytes, 'gif',
+        save_all=True, append_images=frames[1:],
+        duration=20, optimize=True, loop=9999)
+    utilities.delete_temporary_file(bot, cleaned_name)
+    return image_bytes
 
 
 def get_rip(name):
