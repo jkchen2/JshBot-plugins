f3e3533fc9eaad61205de708f36de76c044a7833
jkchen2 <jkchen2@illinois.edu>
2018-04-19 17:53:05 -0500

    Update verification checker

--- fdplugin/fdplugin.py
+++ fdplugin/fdplugin.py
@@ -1,3 +1,4 @@
+import time
 import praw
 import discord
 
@@ -8,8 +9,8 @@ from jshbot import utilities, plugins, configurations, data, logger
 from jshbot.exceptions import ConfiguredBotException
 from jshbot.commands import Command, SubCommand, Shortcut, ArgTypes, Arg, Opt, Response
 
-__version__ = '0.1.0'
-CBException = ConfiguredBotException('r/furry Discord plugin')
+__version__ = '0.1.1'
+CBException = ConfiguredBotException('/r/Furry Discord plugin')
 CBException_vc = ConfiguredBotException('Verification checker')
 uses_configuration = True
 
@@ -47,9 +48,16 @@ def get_commands(bot):
 async def _check_reddit_karma(bot, username):
     """Returns the amount of karma on the r/furry the given user has."""
 
+    verification_period = configurations.get(bot, __name__, 'verification_period')
+    time_threshold = time.time() - (verification_period * 86400)
+
     async def _get_next(generator):
         def _f():
             try:
+                while True:
+                    check = next(generator)
+                    if check.created_utc <= time_threshold:
+                        return check
                 return next(generator)
             except StopIteration:
                 return None
@@ -60,24 +68,24 @@ async def _check_reddit_karma(bot, username):
         return await utilities.future(_f)
 
     # Submission karma using built-in search
-    submission_threshold = configurations.get(bot, __name__, 'submission_karma_threshold')
+    submission_limit = configurations.get(bot, __name__, 'submission_karma_limit')
     submissions = REDDIT_CLIENT.subreddit('furry').search('author:{}'.format(username))
     submission_karma = 0
     while True:
         submission = await _get_next(submissions)
         submission_karma += submission.score if submission else 0
-        if not submission or submission_karma > submission_threshold:
+        if not submission or submission_karma > submission_limit:
             break
 
     # Comment karma
-    comment_threshold = configurations.get(bot, __name__, 'comment_karma_threshold')
+    comment_limit = configurations.get(bot, __name__, 'comment_karma_limit')
     comments = REDDIT_CLIENT.redditor(username).comments.new(limit=None)
     comment_karma = 0
     while True:
         comment = await _get_next(comments)
         if comment and comment.subreddit.display_name == 'furry':
             comment_karma += comment.score if comment else 0
-        if not comment or comment_karma > comment_threshold:
+        if not comment or comment_karma > comment_limit:
             break
 
     return (submission_karma, comment_karma)
@@ -99,19 +107,23 @@ async def verification_check(bot, context):
     """Checks if the given user qualifies for verification by date alone."""
     member = context.arguments[0] or context.author
     verified_role = _get_verified_role(bot, context.guild, member=member)
+    verification_period = configurations.get(bot, __name__, 'verification_period')
 
     # Check that the user has been here for a week
     age = (datetime.now() - member.joined_at).days
-    if age > 7:
-        response = ':white_check_mark: Member for {} days'
+    plural = '' if age == 1 else 's'
+    if age > verification_period:
+        response = ':white_check_mark: Member for {} day{}'
         qualifies = 'qualifies'
+        color = discord.Color(0x77b255)
     else:
-        response = ':x: Member for {} days'
-        qualifies = 'does not qualify'
+        response = ':x: Member for {} day{}'
+        qualifies = 'needs to be a member for at least {} days'.format(verification_period)
+        color = discord.Color(0xdd2e44)
 
     description = '{}\n{} {} for {}'.format(
-        response.format(age), member.mention, qualifies, verified_role.mention)
-    return Response(embed=discord.Embed(description=description))
+        response.format(age, plural), member.mention, qualifies, verified_role.mention)
+    return Response(embed=discord.Embed(description=description, color=color))
 
 
 async def verification_set(bot, context):
@@ -126,25 +138,28 @@ async def verification_karma(bot, context):
     """Checks if the given user has karma in the r/furry subreddit."""
     verified_role = _get_verified_role(bot, context.guild)
     karma = await _check_reddit_karma(bot, context.arguments[0])
+    threshold = configurations.get(bot, __name__, 'karma_threshold')
 
-    # Replace karma amount with threshold if it is past that
-    thresholds = [
-        configurations.get(bot, __name__, 'submission_karma_threshold'),
-        configurations.get(bot, __name__, 'comment_karma_threshold')
+    # Replace karma amount with limits if it is past that
+    limits = [
+        configurations.get(bot, __name__, 'submission_karma_limit'),
+        configurations.get(bot, __name__, 'comment_karma_limit')
     ]
-    zipped = zip(karma, thresholds)
+    zipped = zip(karma, limits)
     karma_strings = [(str(it) if it <= ts else (str(ts) + '+')) for it, ts in zipped]
 
-    if sum(karma) > 0:
+    if sum(karma) >= threshold:
         response = ':white_check_mark: {} submission karma, {} comment karma'
         qualifies = 'qualifies'
+        color = discord.Color(0x77b255)
     else:
         response = ':x: {} submission karma, {} comment karma'
-        qualifies = 'does not qualify'
+        qualifies = 'needs at least {} combined karma'.format(threshold)
+        color = discord.Color(0xdd2e44)
 
     description = '{0}\n[u/{1}](https://www.reddit.com/user/{1}) {2} for {3}'.format(
         response.format(*karma_strings), context.arguments[0], qualifies, verified_role.mention)
-    return Response(embed=discord.Embed(description=description))
+    return Response(embed=discord.Embed(description=description, color=color))
 
 
 @plugins.listen_for('bot_on_ready_boot')
